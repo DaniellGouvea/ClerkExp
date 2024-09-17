0971348f1d7f62cb1fabd8b2c2a0cbbd8bf79c4e
DaniellGouvea <113692743+DaniellGouvea@users.noreply.github.com>
2024-09-17 12:17:04 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,12 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDTdxR-yefxA0sxwcBVa2cq_WXt98Tetls",
-  authDomain: "e-commerce-trabalho-c804a.firebaseapp.com",
-  projectId: "e-commerce-trabalho-c804a",
-  storageBucket: "e-commerce-trabalho-c804a.appspot.com",
-  messagingSenderId: "969518154241",
-  appId: "1:969518154241:web:14418b5810c6a004013600"
+  
 };
 
 // Initialize Firebase
